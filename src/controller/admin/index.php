2334de4a9b8de71e507b598f09c9cee54bbb82b5
Cannabytes <logan22f@gmail.com>
2024-07-13 17:52:11 +0300

    fix last commit

--- src/controller/admin/index.php
+++ src/controller/admin/index.php
@@ -15,11 +15,11 @@ class index {
         validation::user_protection("admin");
 
         $info = server::send(type::SERVER_FULL_INFO)->show()->getResponse();
-
+        $lastCommit = server::send(type::GET_COMMIT_LAST)->show()->getResponse();
         tpl::addVar([
             "title" => lang::get_phrase("admin_panel"),
             "servers" => $info['servers'],
-            "sphere_last_commit" => $info['last_commit'],
+            "sphere_last_commit" => $lastCommit,
             "self_last_commit" => update::getLastCommit(),
         ]);
         tpl::display("admin/index.html");
