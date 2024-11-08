0140f2350ad9ae9d365ff7c5808bf7f5503cb032
Cannabytes <logan22f@gmail.com>
2024-11-08 15:12:37 +0200

    Auto login install

--- src/controller/install/install.php
+++ src/controller/install/install.php
@@ -6,9 +6,9 @@
 namespace Ofey\Logan22\controller\install;
 
 use Exception;
-use Ofey\Logan22\component\alert\board;
 use Ofey\Logan22\component\fileSys\fileSys;
 use Ofey\Logan22\component\redirect;
+use Ofey\Logan22\component\session\session;
 use Ofey\Logan22\component\sphere\server;
 use Ofey\Logan22\component\sphere\type;
 use Ofey\Logan22\component\time\time;
@@ -35,56 +35,56 @@ class install
             die();
         }
         $isHtaccess = file_exists(".htaccess");
-        if ( ! $isHtaccess) {
+        if (!$isHtaccess) {
             self::$allow_install = false;
         }
         tpl::addVar([
-          "need_min_version_php" => version::MIN_PHP_VERSION(),
-          "need_mysql_version"   => version::MIN_MYSQL_VERSION(),
-          "dir_permissions"      => self::checkFolderPermissions(["/data", "/uploads",]),
-          "htaccess"             => $isHtaccess,
-          "isLinux"              => "Linux" == php_uname('s'),
-          "isLocalhost"          => self::is_local_environment(),
-          "php_informations"     => [
-            [
-              "name"  => "PHP_VERSION",
-              "get"   => PHP_VERSION,
-              "min"   => version::MIN_PHP_VERSION(),
-              "allow" => PHP_VERSION >= version::MIN_PHP_VERSION(),
-            ],
-            [
-              "name"  => "upload_max_filesize",
-              "get"   => ini_get("upload_max_filesize"),
-              "min"   => "2M",
-              "allow" => self::compareUploadSizes(ini_get("upload_max_filesize"), "2M"),
-            ],
+            "need_min_version_php" => version::MIN_PHP_VERSION(),
+            "need_mysql_version" => version::MIN_MYSQL_VERSION(),
+            "dir_permissions" => self::checkFolderPermissions(["/data", "/uploads",]),
+            "htaccess" => $isHtaccess,
+            "isLinux" => "Linux" == php_uname('s'),
+            "isLocalhost" => self::is_local_environment(),
+            "php_informations" => [
+                [
+                    "name" => "PHP_VERSION",
+                    "get" => PHP_VERSION,
+                    "min" => version::MIN_PHP_VERSION(),
+                    "allow" => PHP_VERSION >= version::MIN_PHP_VERSION(),
+                ],
+                [
+                    "name" => "upload_max_filesize",
+                    "get" => ini_get("upload_max_filesize"),
+                    "min" => "2M",
+                    "allow" => self::compareUploadSizes(ini_get("upload_max_filesize"), "2M"),
+                ],
 
-          ],
-          "extensions"           => [
-            [
-              "name"  => "gd",
-              "allow" => self::isExtension(extension_loaded('gd') || function_exists('gd_info')),
             ],
-            [
-              "name"  => "curl",
-              "allow" => self::isExtension(extension_loaded('curl')),
+            "extensions" => [
+                [
+                    "name" => "gd",
+                    "allow" => self::isExtension(extension_loaded('gd') || function_exists('gd_info')),
+                ],
+                [
+                    "name" => "curl",
+                    "allow" => self::isExtension(extension_loaded('curl')),
+                ],
+                [
+                    "name" => "pdo_mysql",
+                    "allow" => self::isExtension(extension_loaded('pdo_mysql')),
+                ],
+                [
+                    "name" => "mbstring",
+                    "allow" => self::isExtension(extension_loaded('mbstring')),
+                ],
             ],
-            [
-              "name"  => "pdo_mysql",
-              "allow" => self::isExtension(extension_loaded('pdo_mysql')),
-            ],
-            [
-              "name"  => "mbstring",
-              "allow" => self::isExtension(extension_loaded('mbstring')),
-            ],
-          ],
-          "allow_install"        => self::$allow_install,
+            "allow_install" => self::$allow_install,
         ]);
 
         if ($lang == null || $lang == "") {
             $lang = user::self()->getLang();
         }
-        if ( ! file_exists("src/template/sphere/install/install_{$lang}.html")) {
+        if (!file_exists("src/template/sphere/install/install_{$lang}.html")) {
             $lang = "en";
         }
         tpl::display("install/install_{$lang}.html");
@@ -94,25 +94,25 @@ class install
     {
         $dirPer = [];
         foreach ($dir as $folder) {
-            $permissions      = fileperms(fileSys::get_dir($folder));
+            $permissions = fileperms(fileSys::get_dir($folder));
             $ownerPermissions = ($permissions & 0o700) >> 6;
             $groupPermissions = ($permissions & 0o070) >> 3;
             $otherPermissions = $permissions & 0o007;
             if ($ownerPermissions >= 7 && $groupPermissions >= 5 && $otherPermissions >= 5) {
                 $dirPer[] = [
-                  "path" => $folder,
-                  "per"  => true,
+                    "path" => $folder,
+                    "per" => true,
                 ];
             } else {
                 if (php_uname('s') == "Windows NT") {
                     $dirPer[] = [
-                      "path" => $folder,
-                      "per"  => true,
+                        "path" => $folder,
+                        "per" => true,
                     ];
                 } else {
                     $dirPer[] = [
-                      "path" => $folder,
-                      "per"  => false,
+                        "path" => $folder,
+                        "per" => false,
                     ];
                     self::set_allow_install(false);
                 }
@@ -124,10 +124,10 @@ class install
 
     private static function set_allow_install($b): void
     {
-        if ( ! self::$allow_install) {
+        if (!self::$allow_install) {
             return;
         }
-        if ( ! $b) {
+        if (!$b) {
             self::$allow_install = false;
         }
     }
@@ -136,11 +136,11 @@ class install
     {
         $client_ip = $_SERVER['REMOTE_ADDR'];
         $local_ips = [
-          '127.0.0.1',        // localhost
-          '::1',              // IPv6 localhost
-          '192.168.',         // локальная сеть
-          '10.',              // локальная сеть
-          '172.',              // локальная сеть
+            '127.0.0.1',        // localhost
+            '::1',              // IPv6 localhost
+            '192.168.',         // локальная сеть
+            '10.',              // локальная сеть
+            '172.',              // локальная сеть
         ];
         foreach ($local_ips as $local_ip) {
             if (str_starts_with($client_ip, $local_ip)) {
@@ -153,8 +153,8 @@ class install
 
     private static function compareUploadSizes($size1, $size2): bool
     {
-        $unit1  = strtoupper(substr($size1, -1));
-        $unit2  = strtoupper(substr($size2, -1));
+        $unit1 = strtoupper(substr($size1, -1));
+        $unit2 = strtoupper(substr($size2, -1));
         $value1 = (int)substr($size1, 0, -1);
         $value2 = (int)substr($size2, 0, -1);
         switch ($unit1) {
@@ -181,7 +181,7 @@ class install
 
     private static function isExtension($v)
     {
-        if ( ! $v) {
+        if (!$v) {
             self::set_allow_install(false);
         }
 
@@ -214,35 +214,35 @@ class install
         if ($only_admin) {
             if (auth::get_access_level() != "admin") {
                 echo json_encode([
-                  "type"    => "notice",
-                  "ok"      => true,
-                  "message" => 'Access is denied',
+                    "type" => "notice",
+                    "ok" => true,
+                    "message" => 'Access is denied',
                 ]);
                 exit;
             }
         }
-        $host     = $_POST['host'];
-        $port     = $_POST['port'] ?? 3306;
-        $user     = $_POST['user'];
+        $host = $_POST['host'];
+        $port = $_POST['port'] ?? 3306;
+        $user = $_POST['user'];
         $password = $_POST['password'];
-        $name     = $_POST['name'];
-        $pdo      = \Ofey\Logan22\model\install\install::test_connect_mysql($host, $port, $user, $password, $name);
+        $name = $_POST['name'];
+        $pdo = \Ofey\Logan22\model\install\install::test_connect_mysql($host, $port, $user, $password, $name);
         if ($pdo) {
             $ver = $pdo->query("SELECT VERSION()")->fetchColumn();
             $ver = preg_replace('/^(\d+\.\d+).*$/', '$1', $ver);
             $arr = json_encode([
-              "type"         => "notice",
-              "ok"           => true,
-              "mysqlVersion" => $ver,
-              'message'      => 'Next install',
+                "type" => "notice",
+                "ok" => true,
+                "mysqlVersion" => $ver,
+                'message' => 'Next install',
             ]);
             echo $arr;
             exit;
         } else {
             $data = [
-              'type'    => 'notice',
-              'ok'      => false,
-              'message' => 'Database connection error',
+                'type' => 'notice',
+                'ok' => false,
+                'message' => 'Database connection error',
             ];
             echo json_encode($data);
             exit;
@@ -266,28 +266,28 @@ class install
         if (empty($_POST['email']) || empty($_POST['nickname']) || empty($_POST['adminPassword']) ||
             empty($_POST['host']) || empty($_POST['user']) || empty($_POST['password']) || empty($_POST['name'])) {
             echo json_encode([
-              "type"    => "notice",
-              "ok"      => false,
-              "message" => 'Missing required parameters',
+                "type" => "notice",
+                "ok" => false,
+                "message" => 'Missing required parameters',
             ]);
             exit;
         }
 
-        $email         = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
-        $nickname      = htmlspecialchars($_POST['nickname']);
+        $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+        $nickname = htmlspecialchars($_POST['nickname']);
         $adminPassword = password_hash($_POST['adminPassword'], PASSWORD_BCRYPT);
 
-        $host     = $_POST['host'];
-        $port     = $_POST['port'] ?? 3306;
-        $user     = $_POST['user'];
+        $host = $_POST['host'];
+        $port = $_POST['port'] ?? 3306;
+        $user = $_POST['user'];
         $password = $_POST['password'];
-        $name     = $_POST['name'];
+        $name = $_POST['name'];
 
-        if ( ! $email) {
+        if (!$email) {
             echo json_encode([
-              "type"    => "notice",
-              "ok"      => false,
-              "message" => 'Invalid email format',
+                "type" => "notice",
+                "ok" => false,
+                "message" => 'Invalid email format',
             ]);
             exit;
         }
@@ -307,9 +307,9 @@ class install
             server::tokenDisable(true);
 
             $response = server::send(type::SPHERE_INSTALL, [
-              'filename' => $filenameCheck,
+                'filename' => $filenameCheck,
             ])->show()->getResponse();
-            if ( ! $response['success']) {
+            if (!$response['success']) {
                 throw new Exception("Failed to install sphere");
             }
             $token = $response['token'];
@@ -317,9 +317,9 @@ class install
             unlink($filenameCheck);
         } catch (Exception $e) {
             echo json_encode([
-              "type"    => "notice",
-              "ok"      => false,
-              "message" => "Installation error: " . $e->getMessage(),
+                "type" => "notice",
+                "ok" => false,
+                "message" => "Installation error: " . $e->getMessage(),
             ]);
             exit();
         }
@@ -336,46 +336,44 @@ class install
             $lastCommitData = self::getLastCommitData();
             if ($lastCommitData) {
                 $query = $pdo->prepare(
-                  "INSERT INTO `github_updates` (`sha`, `author`, `url`, `message`, `date`, `date_update`) 
+                    "INSERT INTO `github_updates` (`sha`, `author`, `url`, `message`, `date`, `date_update`) 
                  VALUES (?, ?, ?, ?, ?, ?)"
                 );
                 $query->execute([
-                  $lastCommitData['hash'],
-                  $lastCommitData['author'],
-                  $lastCommitData['url'],
-                  $lastCommitData['message'],
-                  time::mysql(),
-                  time::mysql(),
+                    $lastCommitData['hash'],
+                    $lastCommitData['author'],
+                    $lastCommitData['url'],
+                    $lastCommitData['message'],
+                    time::mysql(),
+                    time::mysql(),
                 ]);
             }
 
             // Вставляем данные администратора
-            $ip  = $_SERVER['REMOTE_ADDR'];
+            $ip = $_SERVER['REMOTE_ADDR'];
             $smt = $pdo->prepare(
-              "INSERT INTO `users` (`name`, `password`, `email`, `ip`, `access_level`)
+                "INSERT INTO `users` (`name`, `password`, `email`, `ip`, `access_level`)
              VALUES (?, ?, ?, ?, ?)"
             );
 
-            $smt->execute([
-              $nickname,
-              $adminPassword,
-              $email,
-              $ip,
-              'admin',
-            ]);
+            $smt->execute([ $nickname, $adminPassword, $email, $ip, 'admin', ]);
+
+            session::add('id', (int)$pdo->lastInsertId());
+            session::add('email', $email);
+            session::add('password', $password);
 
             echo json_encode([
-              "type"    => "notice",
-              'redirect' => "/main",
-              'ok'  => true,
-              'message'  => 'Установлено',
+                "type" => "notice",
+                'redirect' => "/admin",
+                'ok' => true,
+                'message' => 'Установлено',
             ]);
             exit();
         } catch (PDOException|Exception $e) {
             echo json_encode([
-              "type"    => "notice",
-              "ok"      => false,
-              "message" => "Installation error: " . $e->getMessage(),
+                "type" => "notice",
+                "ok" => false,
+                "message" => "Installation error: " . $e->getMessage(),
             ]);
             exit();
         }
@@ -401,7 +399,7 @@ class install
             // Определяем путь к объекту коммита в .git/objects
             $objectPath = sprintf('%s/objects/%s/%s', $gitDir, substr($commitHash, 0, 2), substr($commitHash, 2));
 
-            if ( ! file_exists($objectPath)) {
+            if (!file_exists($objectPath)) {
                 throw new Exception("Object file not found: $objectPath");
             }
 
@@ -409,7 +407,7 @@ class install
             $objectContent = file_get_contents($objectPath);
             $objectContent = gzuncompress($objectContent);
 
-            if ( ! $objectContent) {
+            if (!$objectContent) {
                 throw new Exception("Unable to decompress object content");
             }
 
@@ -422,19 +420,19 @@ class install
         function getLastCommitHash($gitDir): string
         {
             $headFile = $gitDir . '/HEAD';
-            if ( ! file_exists($headFile)) {
+            if (!file_exists($headFile)) {
                 throw new Exception('HEAD file not found in .git directory');
             }
 
             $headContent = file_get_contents($headFile);
             if (strpos($headContent, 'ref: ') === 0) {
-                $refPath        = trim(str_replace('ref: ', '', $headContent));
+                $refPath = trim(str_replace('ref: ', '', $headContent));
                 $commitHashFile = $gitDir . '/' . $refPath;
             } else {
                 return trim($headContent);
             }
 
-            if ( ! file_exists($commitHashFile)) {
+            if (!file_exists($commitHashFile)) {
                 throw new Exception("Commit hash file not found: $commitHashFile");
             }
 
@@ -447,10 +445,10 @@ class install
             $lines = explode("\n", $objectContent);
 
             $commitInfo = [
-              'hash'    => $commitHash,
-              'author'  => '',
-              'date'    => '',
-              'message' => '',
+                'hash' => $commitHash,
+                'author' => '',
+                'date' => '',
+                'message' => '',
             ];
 
             foreach ($lines as $line) {
@@ -458,7 +456,7 @@ class install
                     // Извлекаем автора и дату
                     preg_match('/author (.*) <.*> (\d+) ([+-]\d{4})/', $line, $matches);
                     $commitInfo['author'] = $matches[1];
-                    $commitInfo['date']   = time::mysql();
+                    $commitInfo['date'] = time::mysql();
                 }
                 if (empty($line)) {
                     // Следующая строка будет сообщением коммита
@@ -503,11 +501,11 @@ class install
             $commit = $commits[0];
 
             return [
-              'hash'    => $commit['sha'],
-              'author'  => $commit['commit']['author']['name'],
-              'url'     => $commit['html_url'],
-              'message' => $commit['commit']['message'],
-              'date'    => $commit['commit']['author']['date'],
+                'hash' => $commit['sha'],
+                'author' => $commit['commit']['author']['name'],
+                'url' => $commit['html_url'],
+                'message' => $commit['commit']['message'],
+                'date' => $commit['commit']['author']['date'],
             ];
         } else {
             return null;
