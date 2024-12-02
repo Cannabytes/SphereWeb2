ad5879516ea8a95989ec2ca2a417006f218cdb29
Cannabytes <logan22f@gmail.com>
2024-12-02 03:57:09 +0200

    index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
 ini_set('display_startup_errors', 1);
 ini_set('log_errors', 1);
 //unlink('errors.txt');
