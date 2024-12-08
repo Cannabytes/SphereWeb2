b570c5267095dda72d2444972c496d56264c627b
Cannabytes <logan22f@gmail.com>
2024-12-08 09:55:43 +0200

    update category

--- uploads/sql.php
+++ uploads/sql.php
@@ -1,45 +1,14 @@
 <?php
 
 return [
-    "CREATE TABLE  `support_thread_name` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `thread_name` varchar(600) DEFAULT NULL,
-  `moderators` varchar(1000) DEFAULT NULL,
-  `thread_count` int(11) NOT NULL DEFAULT '0',
-  `weight` int(11) DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;",
 
-    "CREATE TABLE  `support_thread` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `thread_id` int(11) DEFAULT NULL,
-  `owner_id` int(11) DEFAULT NULL COMMENT 'создатель вопроса',
-  `last_user_id` int(11) DEFAULT NULL COMMENT 'ID последний ответ',
-  `last_message_id` int(11) NOT NULL,
-  `private` int(11) NOT NULL DEFAULT '0',
-  `is_close` int(11) NOT NULL DEFAULT '0',
-  `date_update` datetime DEFAULT NULL,
-  `date_create` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;",
-
-    "CREATE TABLE  `support_message` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `thread_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `message` mediumtext,
-  `screens` varchar(1000) NOT NULL,
-  `date_update` datetime DEFAULT NULL,
-  `date_create` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;",
-
-    "CREATE TABLE  `support_message_screen` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `filename` varchar(60) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `date_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;",
+    "INSERT INTO `support_thread_name` (`id`, `thread_name`, `moderators`, `thread_count`, `weight`) VALUES
+    (1, 'account', NULL, 0, 0),
+    (2, 'Client, crash', NULL, 0, 0),
+    (3, 'Server problems', NULL, 0, 0),
+    (4, 'Payment problems', NULL, 0, 0),
+    (5, 'Complaints about players', NULL, 0, 0),
+    (6, 'other', NULL, 1, 0);
+    ",
 
 ];
--- uploads/update_sql.php
+++ uploads/update_sql.php
@@ -4,8 +4,12 @@ class updateSql
 {
     function __construct()
     {
-
         if (file_exists("uploads/sql.php")) {
+            $data = include "uploads/sql.php";
+            if(empty($data)){
+                unlink("uploads/sql.php");
+                return;
+            }
             foreach (include "uploads/sql.php" AS $arr){
                 \Ofey\Logan22\model\db\sql::run($arr);
             }
