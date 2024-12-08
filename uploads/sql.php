8ccb39860a9765424edb37837ced2347cda4ed2c
Cannabytes <logan22f@gmail.com>
2024-12-08 09:59:41 +0200

    update sql

--- uploads/sql.php
+++ uploads/sql.php
@@ -2,13 +2,6 @@
 
 return [
 
-    "INSERT INTO `support_thread_name` (`id`, `thread_name`, `moderators`, `thread_count`, `weight`) VALUES
-    (1, 'account', NULL, 0, 0),
-    (2, 'Client, crash', NULL, 0, 0),
-    (3, 'Server problems', NULL, 0, 0),
-    (4, 'Payment problems', NULL, 0, 0),
-    (5, 'Complaints about players', NULL, 0, 0),
-    (6, 'other', NULL, 1, 0);
-    ",
+    "UPDATE `support_thread_name` SET `thread_count` = 0 WHERE `id` = 6;",
 
 ];
