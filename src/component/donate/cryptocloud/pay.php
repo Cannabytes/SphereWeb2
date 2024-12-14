2d2ca69e31ff0fed62f024afc8e0350c009568e8
Cannabytes <logan22f@gmail.com>
2024-12-14 13:43:53 +0200

    fix cloud pay

--- src/component/donate/cryptocloud/pay.php
+++ src/component/donate/cryptocloud/pay.php
@@ -155,7 +155,7 @@ class cryptocloud extends \Ofey\Logan22\model\donate\pay_abstract
 
         $amount = donate::currency($amount, $this->currency_default);
 
-        self::telegramNotice(user::getUserId($user_id), $_POST['amount'], $this->currency_default, $amount, get_called_class());
+        self::telegramNotice(user::getUserId($user_id), $response['result'][0]['amount_to_pay_usd'], $this->currency_default, $amount, get_called_class());
 
         \Ofey\Logan22\model\admin\userlog::add("user_donate", 545, [$amount, $this->currency_default, get_called_class()]);
         user::getUserId($user_id)->donateAdd($amount)->AddHistoryDonate(amount: $amount, message: null, pay_system:  get_called_class());
