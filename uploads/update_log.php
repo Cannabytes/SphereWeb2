e3c0f5d03558b4201099bdf64f2c77472a25468b
Cannabytes <logan22f@gmail.com>
2024-12-15 23:20:22 +0200

    update

--- uploads/update_log.php
+++ uploads/update_log.php
@@ -2,6 +2,27 @@
 
 return [
     [
+        'date' => '23:19 15.12.2024',
+        'message' => [
+            'ru' => 'В админ-панели вывел данные для просмотра источников трафика на сайт. Там можно узнать кол-во переходов к Вам на сайт (из какого сайта), кол-во тех юзеров сколько зарегалось и сколько их них задонатили.',
+            'en' => 'In the admin panel, I displayed data for viewing traffic sources to the site. There you can find out the number of transitions to your site (from which site), the number of users who registered and how many of them donated.',
+        ],
+    ],
+    [
+        'date' => '17:44 15.12.2024',
+        'message' => [
+            'ru' => 'Теперь можно добавлять помимо стримов YouTube (ссылка на видео), Twitch (ссылка на канал), теперь добавлять можно и Kick.com (ссылкой на канал).',
+            'en' => 'Now you can add, in addition to YouTube streams (link to video), Twitch (link to channel), now you can also add Kick.com (link to channel).',
+        ],
+    ],
+    [
+        'date' => '14:26 13.12.2024',
+        'message' => [
+            'ru' => 'Добавлена возможность получать уведомления событий (донат, бонусы, перевод в игру $) в телеграмм бот.',
+            'en' => 'Added the ability to receive event notifications (donations, bonuses, transfer to the game $) in the telegram bot',
+        ],
+    ],
+    [
         'date' => '11:27 08.12.2024',
         'message' => [
             'ru' => 'Удален тикет и был заменен на Техническую Поддержку.',
