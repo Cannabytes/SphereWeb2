730d63bed71dc618ff09976127ddca83f6192ace
Cannabytes <logan22f@gmail.com>
2025-01-03 07:35:27 +0200

    new table

--- uploads/sql.php
+++ uploads/sql.php
@@ -1,4 +1,11 @@
 <?php
 
 return [
+    'CREATE TABLE `support_read_topics` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `topic_id` int(11) NOT NULL,
+  `read_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+',
 ];
