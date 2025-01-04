813ef35ed686edc48e7f79029034944f46cbddcd
Cannabytes <logan22f@gmail.com>
2025-01-04 14:42:51 +0200

    update

--- uploads/sql.php
+++ uploads/sql.php
@@ -2,11 +2,9 @@
 
 return [
     'ALTER TABLE `support_read_topics`
-     MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,
-     ADD PRIMARY KEY (`id`);
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 ',
     'ALTER TABLE `support_message`
-     MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,
-     ADD PRIMARY KEY (`id`);
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 ',
 ];
