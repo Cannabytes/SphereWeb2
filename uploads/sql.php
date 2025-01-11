9bf5330543d9ae6904c76c42d8d9a6c2f01f5941
Ofey <logan22f@gmail.com>
2025-01-11 14:46:22 +0200

    Описание изменений

--- uploads/sql.php
+++ uploads/sql.php
@@ -1,10 +1,7 @@
 <?php
 
 return [
-    'ALTER TABLE `support_read_topics`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-',
-    'ALTER TABLE `support_message`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-',
+    'ALTER TABLE `users` 
+     ADD COLUMN `last_activity` DATETIME DEFAULT NULL,
+     ADD INDEX `idx_last_activity` (`last_activity`);',
 ];
