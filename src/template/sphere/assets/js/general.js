80e5adbf1fed9ba13c153ed11d8b8f4ed84f37a0
Ofey <logan22f@gmail.com>
2025-01-20 16:11:34 +0200

    update popover

--- src/template/sphere/assets/js/general.js
+++ src/template/sphere/assets/js/general.js
@@ -5,6 +5,12 @@ const tooltipTriggerList = document.querySelectorAll(
 const tooltipList = [...tooltipTriggerList].map(
   (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
+const popoverTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="popover"]'
+);
+const popoverList = [...popoverTriggerList].map(
+    (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+);
 
 function basename(str) {
     var base = new String(str).substring(str.lastIndexOf('\\') + 1);
