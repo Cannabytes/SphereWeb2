33eeaceb0f62a7ca168782ee60eccdae7c457833
Ofey <logan22f@gmail.com>
2025-02-26 05:50:41 +0200

    update

--- uploads/update_log.php
+++ uploads/update_log.php
@@ -2,10 +2,33 @@
 
 return [
     [
+        'date' => '05:37 26.02.2025',
+        'message' => [
+            'ru' => 'Добавлен плагин Розыгрышей (для проф. пользователей)<br>
+                     Добавлен португальский (Бразилия) язык.<br>
+                     Добавлен испанский язык.<br>
+                     Добавлено красивое окно ошибки.',
+            'en' => 'Added Prank plugin (for professional users)<br>
+                     Added Portuguese (Brazil) language.<br>
+                     Added Spanish language.<br>
+                     Added a nice error window.',
+            'es' => 'Se agregó el plugin de Bromas (para usuarios profesionales)<br>
+                     Se agregó el idioma portugués (Brasil).<br>
+                     Se agregó el idioma español.<br>
+                     Se agregó una ventana de error bonita.',
+            'pt' => 'Adicionado o plugin de Pegadinhas (para usuários profissionais)<br>
+                        Adicionado o idioma português (Brasil).<br>
+                        Adicionado o idioma espanhol.<br>
+                        Adicionada uma janela de erro bonita.',
+        ]
+    ],
+    [
         'date' => '14:48 16.02.2025',
         'message' => [
             'ru' => 'Обновлено API платежной системы Freekassa',
             'en' => 'Freekassa payment system API updated',
+            'es' => 'Actualizada la API del sistema de pago Freekassa',
+            'pt' => 'API do sistema de pagamento Freekassa atualizada',
         ]
     ],
     [
@@ -13,6 +36,8 @@ return [
         'message' => [
             'ru' => 'Добавлены новые проверочные IP для платежной системы pally',
             'en' => 'Added new verification IPs for Pally payment system',
+            'es' => 'Se agregaron nuevas IPs de verificación para el sistema de pago Pally',
+            'pt' => 'Adicionados novos IPs de verificação para o sistema de pagamento Pally',
         ]
     ],
     [
@@ -20,6 +45,8 @@ return [
         'message' => [
             'ru' => 'Добавлена возможность создавать кастомные страницы, которые имеют приоритет загрузки. Добавление суффикса custom_(название файла).html',
             'en' => 'Added the ability to create custom pages that have loading priority. Adding the suffix custom_(file name).html',
+            'es' => 'Se agregó la posibilidad de crear páginas personalizadas que tienen prioridad de carga. Agregando el sufijo custom_(nombre del archivo).html',
+            'pt' => 'Adicionada a capacidade de criar páginas personalizadas com prioridade de carregamento. Adicionando o sufixo custom_(nome do arquivo).html',
         ]
     ],
     [
@@ -27,69 +54,89 @@ return [
         'message' => [
             'ru' => 'Добавлен новый плагин форума, можно создавать полноценный форум в личном кабинете.',
             'en' => 'A new forum plugin has been added, you can create a full-fledged forum in your personal account.',
-        ],
+            'es' => 'Se ha añadido un nuevo plugin de foro, puedes crear un foro completo en tu cuenta personal.',
+            'pt' => 'Foi adicionado um novo plugin de fórum, você pode criar um fórum completo na sua conta pessoal.',
+        ]
     ],
     [
         'date' => '22:10 11.02.2025',
         'message' => [
             'ru' => 'Добавлен новый плагин для редактирования стандартных запросов к серверу.',
             'en' => 'Added a new plugin for editing standard server requests',
-        ],
+            'es' => 'Se añadió un nuevo plugin para editar solicitudes estándar al servidor',
+            'pt' => 'Adicionado um novo plugin para edição de solicitações padrão ao servidor',
+        ]
     ],
     [
         'date' => '23:19 15.12.2024',
         'message' => [
             'ru' => 'В админ-панели вывел данные для просмотра источников трафика на сайт. Там можно узнать кол-во переходов к Вам на сайт (из какого сайта), кол-во тех юзеров сколько зарегалось и сколько их них задонатили.',
             'en' => 'In the admin panel, I displayed data for viewing traffic sources to the site. There you can find out the number of transitions to your site (from which site), the number of users who registered and how many of them donated.',
-        ],
+            'es' => 'En el panel de administración, mostré datos para ver las fuentes de tráfico al sitio. Ahí puedes saber el número de transiciones a tu sitio (desde qué sitio), el número de usuarios que se registraron y cuántos de ellos donaron.',
+            'pt' => 'No painel de administração, exibi dados para visualização das fontes de tráfego do site. Lá você pode descobrir o número de transições para o seu site (de qual site), o número de usuários que se registraram e quantos deles doaram.',
+        ]
     ],
     [
         'date' => '17:44 15.12.2024',
         'message' => [
             'ru' => 'Теперь можно добавлять помимо стримов YouTube (ссылка на видео), Twitch (ссылка на канал), теперь добавлять можно и Kick.com (ссылкой на канал).',
             'en' => 'Now you can add, in addition to YouTube streams (link to video), Twitch (link to channel), now you can also add Kick.com (link to channel).',
-        ],
+            'es' => 'Ahora puedes agregar, además de transmisiones de YouTube (enlace al video), Twitch (enlace al canal), también Kick.com (enlace al canal).',
+            'pt' => 'Agora você pode adicionar, além de transmissões do YouTube (link para o vídeo), Twitch (link para o canal), também o Kick.com (link para o canal).',
+        ]
     ],
     [
         'date' => '14:26 13.12.2024',
         'message' => [
             'ru' => 'Добавлена возможность получать уведомления событий (донат, бонусы, перевод в игру $) в телеграмм бот.',
             'en' => 'Added the ability to receive event notifications (donations, bonuses, transfer to the game $) in the telegram bot',
-        ],
+            'es' => 'Se agregó la posibilidad de recibir notificaciones de eventos (donaciones, bonos, transferencia al juego $) en el bot de Telegram',
+            'pt' => 'Adicionada a possibilidade de receber notificações de eventos (doações, bônus, transferência para o jogo $) no bot do Telegram',
+        ]
     ],
     [
         'date' => '11:27 08.12.2024',
         'message' => [
             'ru' => 'Удален тикет и был заменен на Техническую Поддержку.',
             'en' => 'The ticket has been removed and replaced with Technical Support.',
-        ],
+            'es' => 'El ticket ha sido eliminado y reemplazado por Soporte Técnico.',
+            'pt' => 'O ticket foi removido e substituído por Suporte Técnico.',
+        ]
     ],
     [
         'date' => '06:53 02.12.2024',
         'message' => [
             'ru' => 'У некоторые браузеры блокируют открытие новой страницы при переходе для оплаты. Теперь будет добавляться ссылка.',
             'en' => 'Some browsers block opening a new page when going to pay. Now a link will be added.',
-        ],
+            'es' => 'Algunos navegadores bloquean la apertura de una nueva página al ir a pagar. Ahora se añadirá un enlace.',
+            'pt' => 'Alguns navegadores bloqueiam a abertura de uma nova página ao ir para o pagamento. Agora será adicionado um link.',
+        ]
     ],
     [
         'date' => '07:17 27.11.2024',
         'message' => [
             'ru' => 'Обновлены языковые пакеты',
             'en' => 'Language packs updated',
-        ],
+            'es' => 'Paquetes de idiomas actualizados',
+            'pt' => 'Pacotes de idiomas atualizados',
+        ]
     ],
     [
         'date' => '02:06 26.11.2024',
         'message' => [
             'ru' => 'Фикс бага с кэшем, иногда могло показывало что якобы нет игровых персонажей. И уменьшено время обновления актуальных персонажей на аккаунте до 10 сек.',
             'en' => 'Fixed a bug with the cache, sometimes it could show that there were supposedly no game characters. And the update time for current characters on the account was reduced to 10 seconds.',
-        ],
+            'es' => 'Se corrigió un error con el caché, a veces podía mostrar que supuestamente no había personajes de juego. Y se redujo el tiempo de actualización de los personajes actuales en la cuenta a 10 segundos.',
+            'pt' => 'Corrigido um bug com o cache, às vezes podia mostrar que supostamente não havia personagens no jogo. E o tempo de atualização dos personagens atuais na conta foi reduzido para 10 segundos.',
+        ]
     ],
     [
         'date' => '22:27 25.11.2024',
         'message' => [
             'ru' => 'Изменение реферальной системы и вынесение к настройкам сервера. Необходимо будет перенастроить и сохранить реферальную систему.',
             'en' => 'Changing the referral system and moving it to server settings. It will be necessary to reconfigure and save the referral system.',
-        ],
+            'es' => 'Cambio en el sistema de referidos y traslado a la configuración del servidor. Será necesario reconfigurar y guardar el sistema de referidos.',
+            'pt' => 'Mudança no sistema de referência e transferência para as configurações do servidor. Será necessário reconfigurar e salvar o sistema de referência.',
+        ]
     ],
 ];
