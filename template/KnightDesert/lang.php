833ea78aa7bfb5e6a41b59b87ccad473a7dbcd20
Ofey <logan22f@gmail.com>
2025-03-04 06:41:12 +0200

    update

--- template/KnightDesert/lang.php
+++ template/KnightDesert/lang.php
@@ -92,7 +92,131 @@ return [
         'fnav_t_3' => 'Support',
     ],
 
+    'es' => [
+        'nav_home' => 'Inicio',
+        'nav_about' => 'Sobre el servidor',
+        'nav_registration' => 'Registro',
+        'nav_files' => 'Archivos',
+        'nav_statistic' => 'Estadísticas',
+        'nav_forum' => 'Foro',
+        'nav_donate' => 'Donaciones',
+        'nav_account' => 'Cuenta',
+        'nav_auth' => 'Autorización',
+        'nav_desc' => 'Descripción',
+        'nav_rules' => 'Reglas',
+        'nav_help' => 'Ayuda',
+        'nav_know' => 'Base de conocimientos',
 
+        'streams_title' => 'Streams <span class="color-orange">del proyecto</span>',
+        'streams_add' => 'Agregar stream',
+        'stream_desc' => 'Aquí podría estar tu stream',
+        'stream_add' => 'Agregar',
+
+        'servers_title' => 'Nuestros <span class="color-orange">servidores</span>',
+
+        'forum_title' => 'Temas <span class="color-orange">del foro</span>',
+        'forum_author' => 'Autor:',
+
+        'rating_title' => 'Clasificación <span class="color-orange">de los mejores</span>',
+        'rating_clan' => 'Clan',
+        'rating_name' => 'Nombre',
+        'rating_pvp' => 'PvP',
+        'rating_pk' => 'PK',
+        'rating_points' => 'Puntos',
+
+        'events_title' => 'Próximos <span class="color-orange">eventos</span>',
+
+        'footer_title' => 'Servidor de juego <span class="color-orange">L2</span>',
+        'footer_desc' => 'Este servidor es una versión de prueba del juego L2 II y está destinado solo para que los jugadores lo conozcan.',
+
+        'fnav_t_1' => 'Sobre el servidor',
+        'fnav_t_2' => 'Cuenta',
+        'fnav_t_3' => 'Soporte',
+    ],
+
+    'pt' => [
+        'nav_home' => 'Início',
+        'nav_about' => 'Sobre o servidor',
+        'nav_registration' => 'Registro',
+        'nav_files' => 'Arquivos',
+        'nav_statistic' => 'Estatísticas',
+        'nav_forum' => 'Fórum',
+        'nav_donate' => 'Doações',
+        'nav_account' => 'Conta',
+        'nav_auth' => 'Autorização',
+        'nav_desc' => 'Descrição',
+        'nav_rules' => 'Regras',
+        'nav_help' => 'Ajuda',
+        'nav_know' => 'Base de conhecimento',
+
+        'streams_title' => 'Streams <span class="color-orange">do projeto</span>',
+        'streams_add' => 'Adicionar stream',
+        'stream_desc' => 'Aqui poderia estar seu stream',
+        'stream_add' => 'Adicionar',
+
+        'servers_title' => 'Nossos <span class="color-orange">servidores</span>',
+
+        'forum_title' => 'Tópicos <span class="color-orange">do fórum</span>',
+        'forum_author' => 'Autor:',
+
+        'rating_title' => 'Ranking <span class="color-orange">dos melhores</span>',
+        'rating_clan' => 'Clã',
+        'rating_name' => 'Nome',
+        'rating_pvp' => 'PvP',
+        'rating_pk' => 'PK',
+        'rating_points' => 'Pontos',
+
+        'events_title' => 'Próximos <span class="color-orange">eventos</span>',
+
+        'footer_title' => 'Servidor de jogo <span class="color-orange">L2</span>',
+        'footer_desc' => 'Este servidor é uma versão de teste do jogo L2 II e é destinado apenas para a familiarização dos jogadores.',
+
+        'fnav_t_1' => 'Sobre o servidor',
+        'fnav_t_2' => 'Conta',
+        'fnav_t_3' => 'Suporte',
+    ],
+
+    'gr' => [
+        'nav_home' => 'Αρχική',
+        'nav_about' => 'Σχετικά με τον διακομιστή',
+        'nav_registration' => 'Εγγραφή',
+        'nav_files' => 'Αρχεία',
+        'nav_statistic' => 'Στατιστικά',
+        'nav_forum' => 'Φόρουμ',
+        'nav_donate' => 'Δωρεές',
+        'nav_account' => 'Λογαριασμός',
+        'nav_auth' => 'Αυθεντικοποίηση',
+        'nav_desc' => 'Περιγραφή',
+        'nav_rules' => 'Κανόνες',
+        'nav_help' => 'Βοήθεια',
+        'nav_know' => 'Βάση γνώσεων',
+
+        'streams_title' => 'Streams <span class="color-orange">του έργου</span>',
+        'streams_add' => 'Προσθήκη stream',
+        'stream_desc' => 'Εδώ θα μπορούσε να είναι το stream σας',
+        'stream_add' => 'Προσθήκη',
+
+        'servers_title' => 'Οι <span class="color-orange">διακομιστές</span> μας',
+
+        'forum_title' => 'Θέματα <span class="color-orange">του φόρουμ</span>',
+        'forum_author' => 'Συγγραφέας:',
+
+        'rating_title' => 'Κατάταξη <span class="color-orange">των καλύτερων</span>',
+        'rating_clan' => 'Κλαμπ',
+        'rating_name' => 'Όνομα',
+        'rating_pvp' => 'PvP',
+        'rating_pk' => 'PK',
+        'rating_points' => 'Σημεία',
+
+        'events_title' => 'Επερχόμενα <span class="color-orange">γεγονότα</span>',
+
+        'footer_title' => 'Διακομιστής παιχνιδιού <span class="color-orange">L2</span>',
+        'footer_desc' => 'Αυτός ο διακομιστής είναι μια έκδοση δοκιμής του παιχνιδιού L2 II και προορίζεται μόνο για την εξοικείωση των παικτών.',
+
+        'fnav_t_1' => 'Σχετικά με τον διακομιστή',
+        'fnav_t_2' => 'Λογαριασμός',
+        'fnav_t_3' => 'Υποστήριξη',
+    ],
 
 ];
 
