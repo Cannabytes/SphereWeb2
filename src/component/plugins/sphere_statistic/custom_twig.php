5f606f7f0e94d98b06f29d7861d1eda34007f381
Ofey <logan22f@gmail.com>
2025-05-11 02:50:37 +0300

    update

--- src/component/plugins/sphere_statistic/custom_twig.php
+++ src/component/plugins/sphere_statistic/custom_twig.php
@@ -28,11 +28,11 @@ class custom_twig
         }
         $serverStatistic = sql::getRow("SELECT * FROM `statistic_online` WHERE `server_id` = ?", [$serverId]);
         if(!$serverStatistic){
-            $response = server::send(type::SERVER_STATISTIC_ONLINE)->show()->getResponse();
+            $response = server::send(type::SERVER_STATISTIC_ONLINE, ['id' => $serverId])->show(false)->getResponse();
             $data = $response['data'] ?? [];
             sql::run("INSERT INTO `statistic_online` (`server_id`, `count_online_player`, `time`) VALUES (?, ?, ?);", [$serverId, json_encode($data), time::mysql()]);
         }else if(time::diff($serverStatistic['time'],time::mysql()) >= 60*10) {
-            $response = server::send(type::SERVER_STATISTIC_ONLINE)->show()->getResponse();
+            $response = server::send(type::SERVER_STATISTIC_ONLINE, ['id' => $serverId])->show()->getResponse();
             $data = $response['data'] ?? [];
             sql::run("UPDATE `statistic_online` SET `count_online_player` = ?, `time` = ? WHERE `server_id` = ?", [json_encode($data), time::mysql(), $serverId]);
         }else{
