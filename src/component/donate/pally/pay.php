7f74c503efbbb0d64e48d856d658acf032a1ded5
Ofey <logan22f@gmail.com>
2025-06-17 12:12:42 +0300

    upd

--- src/component/donate/pally/pay.php
+++ src/component/donate/pally/pay.php
@@ -25,11 +25,6 @@ class pally extends \Ofey\Logan22\model\donate\pay_abstract
     protected static string $currency_default = 'RUB';
 
     private array $allowIP = [
-        '138.201.53.39',
-        '176.9.155.22',
-        '2.58.70.63',
-        '213.136.76.226',
-        '213.136.84.70',
     ];
 
     public static function inputs(): array
