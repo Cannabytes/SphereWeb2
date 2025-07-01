bcf39e2fc888d22fd5f3d224a4e806cb4dfda7d8
Ofey <logan22f@gmail.com>
2025-07-01 22:47:46 +0300

    upd

--- src/controller/account/characters/hwid.php
+++ src/controller/account/characters/hwid.php
@@ -17,18 +17,26 @@ class hwid
         if ($data) {
             foreach (user::self()->getAccounts() as $account) {
                 if ($account->getAccount() == $account_user) {
+                    $player_id = 0;
+                    foreach ($account->getCharacters() as $character) {
+                        $getPlayerId = $character->getPlayerId();
+                        if ($getPlayerId == $_POST['player_id']) {
+                            $player_id = $getPlayerId;
+                        }
+                    }
                     $server = server::send(type::RESET_HWID, [
                         "account" => $account->getAccount(),
+                        "player_id" => $player_id,
                     ])->show(true)->getResponse();
-                    if (isset($server['success'])) {
+                    if(isset($server['success'])){
                         board::success($server['success']);
                     }
-                    if (isset($server['error'])) {
+                    if(isset($server['error'])){
                         board::error($server['error']);
                     }
                 }
             }
-            board::error("Account not found");
+
         }else{
             board::error("Server not allow reset HWID");
         }
